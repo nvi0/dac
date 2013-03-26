ee7e5ca56fea36ecdec9ebbdf263f7e6083e0a04
nvi <nvi@pdx.edu>
2013-03-26 14:21:51 -0700

    handle file upload at url /upload

--- dac/urls.py
+++ dac/urls.py
@@ -16,4 +16,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    
+    url(r'^fooview/(?P<foo_id>\d+)/$', views.fooview),
+    url(r'^upload/$', views.uploadfile),
 )
--- dac/views.py
+++ dac/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.core.context_processors import csrf
+from models import UploadFileForm
 
 def home(request):
     user_agent = request.META.get("HTTP_USER_AGENT", None)
@@ -8,3 +10,25 @@ def home(request):
         'user_agent': user_agent,
         'ip': ip,
     })
+
+def fooview(request, foo_id):
+    return HttpResponse('from fooview. %s' % foo_id)
+
+def uploadfile(request):
+    if request.method == 'POST':
+        a = request.POST
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+            return HttpResponseRedirect('/')
+    else:
+        form = UploadFileForm()
+
+    c = {'form': form}
+    c.update(csrf(request))
+    return render_to_response('uploadfile.html', c)    
+
+def handle_uploaded_file(file):
+    with open('/tmp/uploaded', 'wb+') as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
